a5fe45a7fab94ee359d8d5f4c741fdd909c977e9
Dennis Gentry <dennis.gentry@gmail.com>
2018-07-16 01:29:51 -0700

    Build on Fedora or Mac.

--- scripts/buildall.sh
+++ scripts/buildall.sh
@@ -3,8 +3,18 @@ if [[ ! $(basename $(pwd)) == "scripts" ]]; then
     pushd scripts
     need_pop=1
 fi
-./prereqs.sh
+
+if [ -f "/etc/redhat-release" ]; then
+    ./prereqs-fedora.sh
+elif [ $(uname) == "Darwin" ]; then
+    ./prereqs-mac.sh
+else
+    echo "Don't know how to build on this OS ($(uname))."
+    exit 1
+fi
+
 ./build.sh --clean && ./create_package.sh --install-to=$HOME/bin
+
 if [[ $need_pop ]]; then
     popd
 fi
