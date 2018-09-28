065f92d61f17e9830655de2c2a52537c4ad1243b
Tomas Soltys <tomas.soltys@range-software.com>
2018-09-28 11:26:19 +0200

    Switched default docker image from Fedora28 to CentOS7

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -10,13 +10,42 @@ myPath=$(getScriptPath)
 prereqsScript=
 
 if [ -f "/etc/redhat-release" ]; then
-    echo_i "Building on $(cat /etc/redhat-release)."
-    prereqsScript="${myPath}/prereqs_fedora.sh"
+    # RedHat based Linux
+    grep -qi "fedora" /etc/redhat-release
+    if [ $? -eq 0 ]; then
+        echo_i "Building on $(cat /etc/redhat-release)"
+        prereqsScript="${myPath}/prereqs_fedora.sh"
+    fi
+    if [ -z "$prereqsScript" ]; then
+        grep -qi "centos" /etc/redhat-release
+        if [ $? -eq 0 ]; then
+            echo_i "Building on $(cat /etc/redhat-release)"
+            prereqsScript="${myPath}/prereqs_centos.sh"
+        fi
+    fi
+    if [ -z "$prereqsScript" ]; then
+        echo_e "Don't know how to build on $(cat /etc/redhat-release)"
+    fi
+elif [ -f "/etc/os-release" ]; then
+    # Suse based Linux
+    . /etc/os-release
+    echo $ID_LIKE | grep -qi "suse"
+    if [ $? -eq 0 ]; then
+        echo_i "Building on $PRETTY_NAME"
+        prereqsScript="${myPath}/prereqs_opensuse.sh"
+    fi
+elif [ -f "/etc/issue" ]; then
+    # Debian based Linux
+    echo_i "Building on $(cat /etc/issue)"
+    prereqsScript="${myPath}/prereqs_debian.sh"
 elif [ $(uname) == "Darwin" ]; then
+    # MacOS
     echo_i "Building on Mac."
     prereqsScript="${myPath}/prereqs_mac.sh"
-else
-    echo_e "Don't know how to build on this OS ($(uname))."
+fi
+
+if [ -z "$prereqsScript" ]; then
+    echo_e "Don't know how to build on this OS ($(uname))"
     exit 1
 fi
 
