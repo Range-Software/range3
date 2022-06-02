c9d2cbd3ba14924b9a77fe3b67b3c6c899969867
Tomas Soltys <tomas.soltys@range-software.com>
2022-06-02 10:44:14 +0200

    Added EPEL repo

--- scripts/prereqs_rhubi.sh
+++ scripts/prereqs_rhubi.sh
@@ -15,6 +15,12 @@ echo_i "Update"
 yum update -y
 assert_success $? "Failed to update" || exit 2
 
+if ! rpm --quiet --query epel-release; then
+    echo_i "Install EPEL repository"
+    yum install -y http://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
+    assert_success $? "Failed to install EPEL repository" || exit 2
+fi
+
 echo_i "Install tools, Qt and ffmpeg"
 yum install -y which sudo make hostname openssh-clients rpm-build gcc-c++ qt6-qtbase-devel qt6-qtsvg ffmpeg ffmpeg-devel
 assert_success $? "Failed to install tools, Qt and ffmpeg" || exit 2
