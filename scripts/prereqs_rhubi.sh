d881f469b955a76590a73088441ed68783698df1
Tomas Soltys <tomas.soltys@range-software.com>
2022-06-02 08:44:04 +0200

    Move from CentOS tho RH UBI

---
+++ scripts/prereqs_rhubi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+getScriptPath () {
+	echo ${0%/*}/
+}
+myPath=$(getScriptPath)
+
+. ${myPath}/lib.sh
+
+echo_i "Update"
+yum update -y
+assert_success $? "Failed to update" || exit 2
+
+echo_i "Install tools, Qt and ffmpeg"
+yum install -y which sudo make hostname openssh-clients rpm-build gcc-c++ qt6-qtbase-devel qt6-qtsvg ffmpeg ffmpeg-devel
+assert_success $? "Failed to install tools, Qt and ffmpeg" || exit 2
+
