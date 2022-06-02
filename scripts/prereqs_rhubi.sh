6b1810f6839b2b733a963f9c459fce9a05cd4ddd
Tomas Soltys <tomas.soltys@range-software.com>
2022-06-02 10:52:02 +0200

    Added rpmfusion to RH UBI

--- scripts/prereqs_rhubi.sh
+++ scripts/prereqs_rhubi.sh
@@ -21,6 +21,18 @@ if ! rpm --quiet --query epel-release; then
     assert_success $? "Failed to install EPEL repository" || exit 2
 fi
 
+if ! rpm --quiet --query rpmfusion-free-release; then
+    echo_i "Install rpmfusion.org repository"
+    yum install -y https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm
+    assert_success $? "Failed to install rpmfusion.org/free repository" || exit 2
+fi
+
+if ! rpm --quiet --query rpmfusion-nonfree-release; then
+    echo_i "Install rpmfusion.org repository"
+    yum install -y https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
+    assert_success $? "Failed to install rpmfusion.org/nonfree repository" || exit 2
+fi
+
 echo_i "Install tools, Qt and ffmpeg"
 yum install -y which sudo make hostname openssh-clients rpm-build gcc-c++ qt6-qtbase-devel qt6-qtsvg ffmpeg ffmpeg-devel
 assert_success $? "Failed to install tools, Qt and ffmpeg" || exit 2
