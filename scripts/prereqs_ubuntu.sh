48e04afcb75348c606d93cff86c117dce151b275
Tomas Soltys <tomas.soltys@range-software.com>
2022-08-14 07:19:44 +0000

    Fixed prereqs for Ubuntu 22.04

--- scripts/prereqs_ubuntu.sh
+++ scripts/prereqs_ubuntu.sh
@@ -16,6 +16,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common
 assert_success $? "Failed to install software-properties-common" || exit 2
 
 echo_i "Install tools, Qt and ffmpeg"
-DEBIAN_FRONTEND=noninteractive apt-get install -y pkg-config build-essential alien openssh-client rpm qtbase5-dev qtpdf5-dev libavutil-dev libavformat-dev libswscale-dev libavdevice-dev ffmpeg
+DEBIAN_FRONTEND=noninteractive apt-get install -y pkg-config build-essential alien openssh-client rpm qt6-base-dev qt6-pdf-dev libqt6svg6-dev libgl1-mesa-dev libavutil-dev libavformat-dev libswscale-dev libavdevice-dev ffmpeg
 assert_success $? "Failed to install tools, Qt and ffmpeg" || exit 2
 
